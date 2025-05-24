b1caf4d0b3f113674f97bb71ba38082536c4d957
lukas <lukas.hartwig@gmx.net>
2025-05-24 13:35:58 +0200

    init

--- lim.c
+++ lim.c
@@ -37,9 +37,6 @@ typedef int16_t i16;
 typedef int32_t i32;
 typedef int64_t i64;
 
-typedef DIR Folder;
-typedef struct dirent Entry;
-
 // ********************* #MISC *********************************/
 
 void die(const char *format, ...) {
@@ -340,26 +337,6 @@ int print_status_line(WINDOW *statArea, GapBuffer *g, int c) {
   //wprintw(statArea, "\t\t\t");
 }
 
-void read_fs(WINDOW *popupArea) {
-  Folder *folder = opendir(".");
-  Entry *entry = NULL;
-  
-  if (!folder) {
-    return;
-  }
-  int l = 0;
-  for (int i = 0; (entry = readdir(folder)) != NULL; i++)
-  int line = 2;
-  for (; (entry = readdir(folder)) != NULL; line++) {
-    mvwprintw(popupArea, line, 3, "%s", entry->d_name);
-  }
-  closedir(folder);
-}
-
-typedef enum {
-  TEXT, OPEN
-} State;
-
 int main(int argc, char **argv) {
 
   initscr();
@@ -373,10 +350,8 @@ int main(int argc, char **argv) {
   WINDOW *lineArea;
   WINDOW *textArea;
   WINDOW *statArea;
-  WINDOW *popupArea;
 
   Screen screen;
-  State state = TEXT;
   GapBuffer g;
   gb_init(&g, 10000);
   ASSERT(g.point < g.cap);
@@ -386,11 +361,6 @@ int main(int argc, char **argv) {
   lineArea = newwin(screen.rows - 1, 4, 0, 0);
   textArea = newwin(screen.rows - 1, screen.cols - 4, 0, 0);
   statArea = newwin(1, screen.cols, 0, 0);
-  
-  popupArea = newwin(5, 30, 10, 10);
-  wresize(popupArea, 30, 60);
-  wbkgd(popupArea, COLOR_PAIR(2));
-  box(popupArea, ACS_VLINE, ACS_HLINE);
   
   mvwin(textArea, 0, 4);
   //vline(ACS_VLINE, screen.rows); // ??
@@ -483,13 +453,6 @@ int main(int argc, char **argv) {
       changed = true;
     }
 
-    else if (c == CTRL('r')) {
-      if (state == TEXT)
-        state = OPEN;
-      else if (state == OPEN)
-        state = TEXT;
-    }
-
     // else if (c == 127) {
     // }
 
@@ -501,10 +464,6 @@ int main(int argc, char **argv) {
     }
     wrefresh(textArea);
     wmove(textArea, g.lin, g.col);
-    if (state == OPEN) {
-      read_fs(popupArea);
-      wrefresh(popupArea);
-    }
   }
   
   clear();
